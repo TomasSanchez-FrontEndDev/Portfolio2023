836e0db8ece0119ce829159f3465381953489be1
Kaise <tomasgonzalosanchez1@gmail.com>
2023-02-08 20:23:11 -0300

    Changes

--- src/components/Especial_components/Hamburguer.jsx
+++ src/components/Especial_components/Hamburguer.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import Squash from 'hamburger-react'
+import React, { useEffect, useState } from 'react';
+import Squash from 'hamburger-react';
 
 const Hamburguer = () => {
-  var navbar = document.getElementById("NavBar");
+  /*var navbar = document.getElementById('NavBar');*/
+  const [navbar,setNavbar] = useState(null);
+  useEffect(() => {
+    const newNavbar = document.getElementById('NavBar');
+    setNavbar(newNavbar);
+  }, []);
   return (
-    <Squash onToggle={toggled => {
-      if (toggled) {
-         navbar.classList.add("closeNavbar")
-      } else {
-         navbar.classList.remove("closeNavbar")
-      }
-    }} />
-  )
-}
+    <Squash
+      onToggle={(toggled) => {
+        console.log(toggled);
+        if (toggled) {
+          navbar.classList.add('closeNavbar');
+        } else {
+          navbar.classList.remove('closeNavbar');
+        }
+      }}
+    />
+  );
+};
 
 export default Hamburguer;
