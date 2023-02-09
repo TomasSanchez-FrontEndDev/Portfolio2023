62c68c5205c4720cfac5779932794d5562ce2768
Kaise <tomasgonzalosanchez1@gmail.com>
2023-02-09 18:23:23 -0300

    a

--- src/components/Especial_components/Changes_waves.jsx
+++ src/components/Especial_components/Changes_waves.jsx
@@ -3,9 +3,26 @@ import React from 'react';
 const Changes_waves = () => {
   return (
     <div>
-      
+
     </div>
   );
 };
 
 export default Changes_waves
+
+    /* 
+    const wave1 = document.getElementById('wave1');
+    const wave2 = document.getElementById('wave2');
+    const wave3 = document.getElementById('wave3');
+    const wave4 = document.getElementById('wave4');
+    
+    wave1.src = wave1_after;
+    wave2.src = wave2_after;
+    wave3.src = wave3_after;
+    wave4.src = wave4_after;
+
+    wave1.scr = waveInitial;
+    wave2.scr = wave2Initial;
+    wave3.scr = wave3Initial;
+    wave4.scr = wave4Initial;
+    */
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import EmailIcon from '../img/Mail.png';
 import LinkedinIcon from '../img/LinkedinIcon.png';
 import WhatsappIcon from '../img/WhatsappIcon.png';
+import github from '../img/github.png';
+import linkedin from '../img/linkedin.png';
+import upwork from '../img/upwork.png';
 
 const Footer = () => {
   return (
-    <div className='bg-gradient-to-b from-[#72EFDD] to-[#72EF9C] pb-8 font-[poppins] px-3 md:px-0' id='Footer' >
+    <div
+      className='bg-gradient-to-b from-[#72EFDD] to-[#72EF9C] pb-8 font-[poppins] px-3 md:px-0'
+      id='Footer'
+    >
       <div className='flex justify-evenly'>
         <div className='bg-[#313036] flex flex-col w-14 p-2 rounded-2xl space-y-3'>
           <a href=''>
@@ -20,17 +26,19 @@ const Footer = () => {
         </div>
         <div className='text-center flex flex-col justify-evenly text-[#313036]'>
           <p className='leading-10 text-4xl font-semibold'>Tomas Sanchez</p>
-          <p className='leading-9 font-light text-1xl'>All rights reserved 2022</p>
+          <p className='leading-9 font-light text-1xl'>
+            All rights reserved 2022
+          </p>
         </div>
         <div className='bg-[#313036] flex flex-col w-14 p-2 rounded-2xl space-y-3'>
           <a href=''>
-            <img src={EmailIcon} alt='' />
+            <img src={github} alt='' />
           </a>
           <a href=''>
             <img src={LinkedinIcon} alt='' />
           </a>
           <a href=''>
-            <img src={WhatsappIcon} alt='' />
+            <img src={upwork} alt='' />
           </a>
         </div>
       </div>
