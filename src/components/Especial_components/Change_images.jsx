659c7cc8539e94ec7b66f979d65c0f1efb951de7
Kaise <tomasgonzalosanchez1@gmail.com>
2023-02-09 17:27:46 -0300

    a

--- src/components/Especial_components/Change_images.jsx
+++ src/components/Especial_components/Change_images.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Change_images = () => {
+function Appn() {
+  const [imageSrc, setImageSrc] = useState("image1.jpg");
+
+  const handleClick = () => {
+    if (imageSrc === "image1.jpg") {
+      setImageSrc("image2.jpg");
+    } else {
+      setImageSrc("image1.jpg");
+    }
+  };
+
   return (
-    <div>Change_images</div>
-  )
+    <div>
+      <img src={imageSrc} alt="Current Image" />
+      <br />
+      <button onClick={handleClick}>Change Image</button>
+    </div>
+  );
 }
 
-export default Change_images
+export default Appn;
